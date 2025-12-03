f9187bf1205526fcb99b973e018bda5fea4f8fa0
SoYeong <gaya1273@naver.com>
2025-12-03 16:36:06 +0900

    init: 스프링 프로젝트 초기화

---
+++ src/test/java/cloud/memome/memome_backend/MemomeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package cloud.memome.memome_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemomeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
