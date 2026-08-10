dabbc87dc2f66dd2e0b27fa1d1c6661282d76af0
soyeong Lee <gaya1273@naver.com>
2026-08-10 22:39:39 +0900

    fix: mysql 초기화 안되는 문제 해결
    - mysql 초기화 안되는 문제(id 생성전략 불일치, 테이블명 대소문자 불일치)해결 위해 Member, Memo 수정

--- src/main/java/cloud/memome/backend/domain/memo/Memo.java
+++ src/main/java/cloud/memome/backend/domain/memo/Memo.java
@@ -7,21 +7,24 @@ import cloud.memome.backend.domain.memo.exception.NotMemoOwnerException;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.Lob;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
+@Table(name = "memo")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
 public class Memo {
 	@Id
-	@GeneratedValue
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String title;
 	@Lob
